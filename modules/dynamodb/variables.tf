0d1d67de837058f71cc63738fba869458f4a4a5a
JoePallister <joepallister@gmail.com>
2026-06-06 13:27:46 +0100

    absorbing old apigateway example as baseline

---
+++ modules/dynamodb/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  type    = string
+  default = "releases"
+}
+
+variable "hash_key" {
+  type    = string
+  default = "service"
+}
+
+variable "hash_key_type" {
+  type    = string
+  default = "S"
+}
